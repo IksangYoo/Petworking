a3c54b87817db26dbb0660d4ae36153bbd075ae5
Iksang Yoo <andrew4477@naver.com>
2022-05-31 01:51:56 -0400

    Add changing feed display mode

--- Petwork/Controllers/ResultsViewController.swift
+++ Petwork/Controllers/ResultsViewController.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-class ResultsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ResultsViewController: UIViewController {
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var segmentIndex = 0
+    var whichButtonClicked = "left"
     var searchTerm: String = ""
     let images = (1..<19).map { UIImage(named: "img_movie_\($0)") }
     
@@ -16,33 +21,47 @@ class ResultsViewController: UIViewController, UICollectionViewDataSource, UICol
     override func viewDidLoad() {
         super.viewDidLoad()
         resultLabel.text = "Results for \(searchTerm)"
+        collectionView.register(UINib(nibName: "ResultCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "feedCell")
     }
     
+    @IBAction func switchDisplayMode(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            segmentIndex = sender.selectedSegmentIndex
+            collectionView.reloadData()
+        } else {
+            segmentIndex = sender.selectedSegmentIndex
+            collectionView.reloadData()
+        }
+    }
+    
+}
+
+extension ResultsViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "feedImageCell", for: indexPath) as? FeedImageCell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "feedCell", for: indexPath) as? ResultCollectionViewCell
         else {
             return UICollectionViewCell()
         }
-        cell.feedImage.image = images[indexPath.row]
+        cell.feedImageView.image = images[indexPath.row]
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var width = collectionView.frame.width
+        let height : CGFloat
+        let inset : CGFloat = 2
         
-        let width = (collectionView.frame.width ) / 3
-        let height = width
-        
-        
-        return CGSize(width: width, height: height)
-        
+        if segmentIndex == 0 {
+            width = (width / 3) - inset * 2
+            height = width
+            return CGSize(width: width, height: height)
+        } else {
+            height = width
+            return CGSize(width: width, height: height)
+        }
     }
-    
-}
-
-class FeedImageCell: UICollectionViewCell {
-    @IBOutlet weak var feedImage: UIImageView!
 }
---
+++ Petwork/Views/ResultCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ResultCollectionViewCell.swift
+//  Petwork
+//
+//  Created by Iksang Yoo on 2022/05/30.
+//
+
+import UIKit
+
+class ResultCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var feedImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
